6facfd22251c790c47dcd07c889368ab8aab71a8
Sam Jackson <sjackson@drakeent.com>
2018-01-29 15:39:13 -0500

    Move to track car

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 
     $("#video").on("error", function(){
-        $(this).attr('src', '/video-placeholder.png');
+        $(this).attr('src', './video-placeholder.png');
     });
 
     $('#video').attr('src', window.location.protocol + '//' + window.location.hostname + '/vid');
@@ -9,19 +9,15 @@ $(document).ready(function(){
     var reOrientate = function(){
         if(screen.orientation.angle == 0){
             $('#center').attr('class', 'center-vertical');
-            $('.side').css('min-height', '120vh');
-            $('.inner').css('top', '52%');
-            $('.outter').css('top', '40%');
+            $('.side').attr('class', 'side side-vertical');
             setTimeout(function(){
                 var imgHeight = $('img#video').css('height');
                 $('#stat').css('top', imgHeight);
             }, 500);
         } else {
             $('#center').attr('class', 'center-horizontal');
-            $('.side').css('min-height', '100vh');
             $('#stat').css('top', '0px');
-            $('.inner').css('top', '40%');
-            $('.outter').css('top', '15%');
+            $('.side').attr('class', 'side side-horizontal');
         }
     }
 
@@ -47,87 +43,64 @@ $(document).ready(function(){
         window.location = window.location.protocol + '//' + window.location.host;
     });
 
-    //FWD
-    $('#fwd').on('tapstart', function(e){
-        $(this).css('background-color', 'rgb(100,100,255)');
-        $('#rev').css('background-color', 'rgb(169,169,169)');
-        $(this).css('outline', 'none');
-        $(this).css('border', 'none');
-        movement.Forward = 1;
-        movement.Reverse = 0;
-        Move();
-    });
-
-    $('#fwd').on('tapend', function(e){
-        $(this).css('background-color', 'rgb(169, 169, 169)');
-        $(this).css('outline', 'none');
-        $(this).css('border', 'none');
-        movement.Forward = 0;
-        Move();  
-    });
-
-    //REV
-    $('#rev').on('tapstart', function(e){
-        $(this).css('background-color', 'rgb(100,100,255)');
-        $('#fwd').css('background-color', 'rgb(169,169,169)');
-        $(this).css('outline', 'none');
-        $(this).css('border', 'none');
-        movement.Reverse = 1;
-        movement.Forward = 0;
-        Move();
-    });
-
-    $('#rev').on('tapend', function(e){
-        $(this).css('background-color', 'rgb(169, 169, 169)');
-        $(this).css('outline', 'none');
-        $(this).css('border', 'none');
-        movement.Reverse = 0;
-        Move();
-    });
-
     //LEFT
-    $('#left').on('tapstart', function(e){
-        $(this).css('background-color', 'rgb(100,100,255)');
-        $('#right').css('background-color', 'rgb(169,169,169)');
-        $(this).css('outline', 'none');
-        $(this).css('border', 'none');
-        movement.Left = 1;
-        movement.Right = 0;
-        Move();
+    var left = document.getElementById('left');
+    
+    noUiSlider.create(left, {
+        start: [0],
+        direction: 'rtl',
+        orientation: 'vertical',
+        behaviour: 'tap-drag',
+        multitouch: true,
+        step: 1,
+        range: {
+            'min': -10,
+            'max': 10
+        }
     });
 
-    $('#left').on('tapend', function(e){
-        $(this).css('background-color', 'rgb(169, 169, 169)');
-        $(this).css('outline', 'none');
-        $(this).css('border', 'none');
-        movement.Left = 0;
-        Move();
+    left.noUiSlider.on('slide', function(){
+        var l = left.noUiSlider.get();
+        $('#leftDiv').html("Left: " + l);
+    });
+
+    left.noUiSlider.on('end', function(){
+        left.noUiSlider.set(0);
+        $('#leftDiv').html("Left: " + 0);
     });
 
     //RIGHT
-    $('#right').on('tapstart', function(e){
-        $(this).css('background-color', 'rgb(100,100,255)');
-        $('#left').css('background-color', 'rgb(169,169,169)');
-        $(this).css('outline', 'none');
-        $(this).css('border', 'none');
-        movement.Right = 1;
-        movement.Left = 0;
-        Move();
+    var right = document.getElementById('right');
+    
+    noUiSlider.create(right, {
+        start: [0],
+        direction: 'rtl',
+        orientation: 'vertical',
+        behaviour: 'tap-drag',
+        multitouch: true,
+        step: 1,
+        range: {
+            'min': -10,
+            'max': 10
+        }
     });
 
-    $('#right').on('tapend', function(e){
-        $(this).css('background-color', 'rgb(169, 169, 169)');
-        $(this).css('outline', 'none');
-        $(this).css('border', 'none');
-        movement.Right = 0;
-        Move();
+    right.noUiSlider.on('slide', function(){
+        var r = right.noUiSlider.get();
+        $('#rightDiv').html("Right: " + r);
     });
-    
+
+    right.noUiSlider.on('end', function(){
+        right.noUiSlider.set(0);
+        $('#rightDiv').html("Right: " + 0);
+    });
+
     var ws;
     var wsUri = "ws:";
     if (window.location.protocol === "https:") { wsUri = "wss:"; }
     wsUri += "//" + window.location.host + window.location.pathname.replace('picar', '') + "ws/picar";
 
+    /*
     ws = new WebSocket(wsUri);
     
     ws.onmessage = function(msg) {
@@ -149,6 +122,7 @@ $(document).ready(function(){
         // in case of lost connection tries to reconnect every 3 secs
         setTimeout(wsConnect,3000);
     }
+    */
 
     $('.menu-btn').on('click', function() {
         if($('.menu').css('width') === '0px'){
